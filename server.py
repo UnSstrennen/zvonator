9a3b34e479d02534c3a6f68789745b90c7cf435c
Un Sstrennen <n-i-k-i-t@yandex.ru>
2019-06-17 12:35:57 +0300

    upd

--- server.py
+++ server.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template, request, redirect
 from werkzeug.utils import secure_filename
 from main import ring, db, config
-from os import path
+import os
+from string import punctuation
 
 
 UPLOAD_DIR = config['play_settings']['sound_directory']
+SUPPORTED_FORMATS = config['play_settings']['supported_formats'].split()
 
 
 app = Flask(__name__)
@@ -26,18 +28,40 @@ def switch_ring():
     return redirect('/')
 
 
-@app.route('/upload', methods=['GET', 'POST'])
+def valid_filename(filename):
+    """ returns OK as string if filename is valid, else it returns error description as string """
+    if len(filename) == 0:
+        return 'Пустое имя файла!'
+    if filename[0] in punctuation:
+        return 'Имя файла не должно начинаться со знаков препинания!'
+    format = filename.split('.')
+    if format not in SUPPORTED_FORMATS:
+        return 'Формат файла не поддерживается.'
+    return 'OK'
+
+
+@app.route('/upload', methods=['GET', 'POST', 'DELETE'])
 def upload():
     if request.method == 'GET':
         return render_template('upload.html')
     elif request.method == 'POST':
         file = request.files['file']
         print(request.form.get('hours'))
-        return '11111'
         if file:
             filename = secure_filename(file.filename)
-            file.save(path.join(UPLOAD_DIR, filename))
+            valid = valid_filename(filename)
+            file.save(os.path.join(UPLOAD_DIR, filename))
         return 'OK'
+    elif request.method == 'DELETE':
+        try:
+            track_to_delete = request.json['name']
+            path = UPLOAD_DIR + track_to_delete
+            os.remove(path)
+            return 'OK'
+        except KeyError:
+            return 'ERROR: Bad request'
+        except FileNotFoundError:
+            return 'ERROR: There is no file named {} on the server'.format(track_to_delete)
 
 
 if __name__ == '__main__':
